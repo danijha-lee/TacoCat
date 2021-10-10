2b89006d0ff69b325e30fade96ce921030258f62
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-10-10 01:57:05 -0400

    done

--- js/site.js
+++ js/site.js
@@ -1,31 +1,71 @@
-function displayMessage() {
-    let msg = "";
+function getWord() {
+    //access the webpage  and gets the value from the inputs
+    let word = document.getElementById("word").value;
 
-    msg = document.getElementById("message").value;
+    //takes away capitilization, special characters and spaces from the word
+    let modifiedWord = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
 
-    alert(msg);
+    //takes the word and flips it and sets it to a value
+    let revWord = flipWord(modifiedWord);
+
+    //setting up boolean and an empty string for error codes
+    let errorState = false;
+    let errorMsg = "";
+
+    //this becomes true if the word and it's palindrome doesn't equal
+    if (revWord != modifiedWord) {
+       return Swal.fire({
+           icon: 'error',
+           title: `Oops... ${word} is not a palindrome`,
+           text: "Your word reversed is " + revWord,
+           footer: '<a target="_blank" href="https://en.wikipedia.org/wiki/Palindrome">Why not?</a>'
+       })
+       
+
+    }
+
+    //this becomes true if they didn't put enough letters to have a palindrome
+    if (modifiedWord.length < 2) {
+       return Swal.fire({
+           icon: 'error',
+           title: 'Oops...',
+           text: "You didn't add enough letters for this.",
+           footer: '<a target="_blank" href="https://en.wikipedia.org/wiki/Palindrome">Why not?</a>'
+       })
+       
+    }
+
+    //if any of the previous becomes true, a SweetAlert will fire and exit the function
+  
+
+    //take the word and it's plaindrome and pass it to a function that will display it
+    displayResults(word, revWord);
 }
 
+//function that takes the value and make a string that is the word reversed
+function flipWord(word) {
 
-function displayMessage2() {
-    let msg = "";
+    //creates an empty string
+    let revWord = "";
 
-    msg = document.getElementById("message").value;
+    //for every letter in the word, create a word with the letters in reverse
+    for (let index = word.length - 1; index >= 0; index--) {
+        let letter = word[index];
 
-    Swal.fire(msg);
+        revWord += letter;
+    }
+
+    return revWord;
 }
 
-
-// Two functions
-// function displayMessage(msg) {
-
-//     alert(msg);
-// }
-
-// function getMessage() {
-//     let msg = "";
-
-//     msg = document.getElementById("message").value;
-
-//     displayMessage(msg);
-// }
+//take the words and display the words to the webpage
+function displayResults(word, revWord) {
+   
+    return Swal.fire({
+        icon: 'success',
+        title: 'Congrats!',
+        text: ` ${word} is a palindrome!
+            ${word} Reversed is ${revWord}.`
+        
+    })
+}
